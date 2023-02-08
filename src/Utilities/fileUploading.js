64c48d6a3407968f7fdbdc6c6b892a1c06362c72
MohamedMSaber <MohamedMSaber@outlook.com>
2023-02-09 00:01:52 +0200

    refactor file upload function

--- src/Utilities/fileUploading.js
+++ src/Utilities/fileUploading.js
@@ -1,8 +1,7 @@
 const multer = require('multer');
 const AppError = require('./AppError');
 
-
-exports.uploadingSingleFile=(filedName , folderName)=>{
+let options = (folderName)=>{
     const storage = multer.diskStorage({
         destination: function (req, file, cb) {
             cb(null, `Uploads/${folderName}`)
@@ -21,27 +20,10 @@ exports.uploadingSingleFile=(filedName , folderName)=>{
         }
     }
     const upload = multer({ storage: storage, fileFilter: fileFilter })
-    return upload.single(filedName);
+    return upload;
 }
 
-exports.uploadingMultiFile = (fildesArray, folderName) => {
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, `Uploads/${folderName}`)
-        },
-        filename: function (req, file, cb) {
-            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-            cb(null, uniqueSuffix + '-' + file.originalname)
-        }
-    })
-    function fileFilter(req, file, cb) {
-        if (file.mimetype.startsWith('image')) {
-            cb(null, true)
-        }
-        else {
-            cb(new AppError("Images Only", 400), false)
-        }
-    }
-    const upload = multer({ storage: storage, fileFilter: fileFilter })
-    return upload.fields(fildesArray);
-}
+exports.uploadingSingleFile=(filedName , folderName) => options(folderName).single(filedName);
+
+
+exports.uploadingMultiFile = (fildesArray, folderName) => options(folderName).fields(fildesArray);
---
+++ src/components/Product/product.model.js
@@ -0,0 +1,76 @@
+const { Schema, model, Types } = require("mongoose");
+const schema = Schema({
+    name: {
+        type: String,
+        required: [true, "Product name required"],
+        trim: true,
+        unique: [true, "Product name unique"],
+        minlength: [2, "too short Product name"],
+    },
+    slug: {
+        type: String,
+        lowercase: true,
+    },
+    description: {
+        type: String,
+        required: [true, "Product description required"],
+        trim: true,
+        minlength: [10, "too short Product description"],
+    },
+    quantity: {
+        type: Number,
+        required: [true, "Product quantity required"],
+        default: 0,
+    },
+    colors: [String],
+    price: {
+        type: Number,
+        required: [true, "Product price required"],
+    },
+    priceAfterDiscount: {
+        type: Number,
+        required: [true, "Product price after discount required"],
+    },
+    sold: {
+        type: Number,
+        required: [true, "Product sold required"],
+        default: 0,
+    },
+    imageCover: String,
+    images: [String],
+    category: {
+        type: Types.ObjectId,
+        ref: "category",
+        required: [true, "Product category required"],
+    },
+    subcategory: {
+        type: Types.ObjectId,
+        ref: "subcategory",
+        required: [true, "Product subcategory required"],
+    },
+    brand: {
+        type: Types.ObjectId,
+        ref: "brand",
+        required: [true, "Product brand required"],
+    },
+    ratingAverage: {
+        type: Number,
+        min: [1, "ratingAverage must be greater than 1"],
+        min: [5, "ratingAverage must be less than 5"],
+    },
+    ratingCount: {
+        type: Number,
+        default: 0,
+    },
+}, { timestamps: true });
+schema.post('init', (doc) => {
+    let images = [];
+    doc.imageCover = `${process.env.BASE_URL}` + "/Product/" + doc.imageCover;
+    doc.images.forEach((img)=>{
+        images.push(`${process.env.BASE_URL}` + "/Product/" + img)
+    })
+    doc.images = images;
+})
+
+
+module.exports = model("product", schema);
